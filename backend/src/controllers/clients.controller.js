f5d7b0c4943ca841d325f59d96d0c8a7f6b269f1
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-24 23:13:11 -0500

    operaciones con la base de datos

---
+++ backend/src/controllers/clients.controller.js
@@ -0,0 +1,11 @@
+export const createClient = (req, res) => {};
+
+export const getClients = (req, res) => {
+  res.json("get Clients");
+};
+
+export const getClientById = (req, res) => {};
+
+export const updateClientById = (req, res) => {};
+
+export const deleteClientById = (req, res) => {};

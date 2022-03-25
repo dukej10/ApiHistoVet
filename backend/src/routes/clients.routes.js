30bba859c2670de1a503c732f8a4c91d5311525d
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-24 23:13:25 -0500

    rutas con clientes

---
+++ backend/src/routes/clients.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+const router = Router();
+
+// Controllers
+import * as clientsCtrl from "../controllers/clients.controller";
+
+router.get("/", clientsCtrl.getClients);
+router.post("/", clientsCtrl.createClient);
+router.get("/:clientId", clientsCtrl.getClientById);
+router.put("/:clientId", clientsCtrl.updateClientById);
+router.delete("/:clientId", clientsCtrl.deleteClientById);
+
+export default router;
